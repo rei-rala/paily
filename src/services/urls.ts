9c9c5a5ab185f5bb11245de1ac3eaf85994325a7
Ramon <67976310+rei-rala@users.noreply.github.com>
2023-01-30 21:15:11 -0300

    ok

--- src/services/urls.ts
+++ src/services/urls.ts
@@ -1,3 +1,3 @@
-export const API_BASEURL = process.env.NODE_ENV === 'production' ? process.env.API_BASEURL : 'http://192.168.0.130:8080'
+export const API_BASEURL = process.env.NODE_ENV === 'production' ? ""+process.env.API_BASEURL : 'http://192.168.0.130:8080'
 export const URL_USERS = API_BASEURL + '/api/user'
 export const URL_CRIPTO = API_BASEURL + '/api/cripto'
